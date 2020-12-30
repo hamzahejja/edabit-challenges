fdf5509565265b31fe4a7daa74d1206ebf5e77e3
Hamza Hejja <hamzahejja@hotmail.com>
2020-12-30 11:25:14 +0200

    JS(Expert): Seating Students JavaScript Challenge

--- src/challenge.js
+++ src/challenge.js
@@ -2356,3 +2356,32 @@ function sum(arr) {
     return sum + innerSum;
   }, 0)
 }
+
+/**
+ * Create a function that reads an array of integers stored which will be in the following format:
+ * [[K, r1, r2, r3, ...]], where K represents the number of desks in a classroom,
+ * and the rest of the integers in the array will be in sorted order and will represent
+ * the desks that are already occupied. All of the desks will be arranged in two columns,
+ * where desk #1 is at the top left, desk #2 at the top right,
+ * desk #3 is below #1, desk #4 is below #2, etc.
+ * Your program should return the number of ways two students can be seated next to each other.
+ * This means one student is on the left and one on the right,
+ * or one student is directly above or below the other student.
+ *
+ * @param {Array} arr
+ * @returns {number}
+ */
+function seatingStudents(arr) {
+  const [k, ...occupiedDesks] = arr;
+  const vacantDesks = Array.from({ length: k }, (_, i) => i + 1)
+    .filter(desk => !occupiedDesks.includes(desk))
+    .sort((vd1, vd2) => vd1 - vd2);
+  const possibleSeatings = vacantDesks.reduce((acc, v, i) => {
+    return acc.concat(vacantDesks.slice(i + 1).map(w => [v, w]))
+  }, []);
+
+  return possibleSeatings.filter(([vd1, vd2]) => {
+    return Math.abs(vd1 - vd2) === 2 ||
+      (vd1 % 2 !== 0 && vd2 % 2 === 0 && Math.abs(vd1 - vd2) === 1);
+  }).length;
+}
